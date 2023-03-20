71a7d03072f801a30aa11d4fb042b5385ff6d4b1
Samuel Majerus <ladolego@gmail.com>
2023-03-20 13:05:24 -0700

    Test 1, logic -- Passed.

--- src/isOdd.js
+++ src/isOdd.js
@@ -1,12 +1,12 @@
 export default function isOdd(binRep) {
-  // //Assume 'binRep' is a string consisting of 0's and 1's. 
-  // let lastDigit = binRep[binRep.length-1];
+  //Assume 'binRep' is a string consisting of 0's and 1's. 
+  let lastDigit = binRep[binRep.length-1];
 
-  // console.log("Last digit of 'binRep' is:  " + lastDigit);
+  console.log("Last digit of 'binRep' is:  " + lastDigit);
 
-  // if (lastDigit === '0') {
-  //   return false; 
-  // } else {
-  //   return true; 
-  // } 
+  if (lastDigit === '0') {
+    return false; 
+  } else {
+    return true; 
+  } 
 }
